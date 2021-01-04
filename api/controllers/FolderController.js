346145ac959c989751f3bc99ffec76b7c155323c
Doan Viet Hoang <hoangdv257@gmail.com>
2021-01-04 09:45:38 +0700

    fix add sets

--- api/controllers/FolderController.js
+++ api/controllers/FolderController.js
@@ -1,5 +1,5 @@
 const { set } = require("grunt");
-const { object } = require("underscore");
+const { object, string } = require("underscore");
 
 module.exports = {
     createFolder: async function (req, res) {
@@ -14,29 +14,29 @@ module.exports = {
         res.redirect(`/${user.username}/folders/${newFolder.id}`);
     },
 
-    getFolder: async function(req, res){
+    getFolder: async function (req, res) {
         var folderId = req.params.folderId;
         var folder = await sails.models.folder.findOne({ id: folderId });
         var setsInFolder = await sails.models.folder.findOne({ id: folderId }).populate('sets');
         var curUser = await sails.models.user.findOne({ username: req.signedCookies.user.username });
         var curUserSets = await sails.models.set.find({ user_id: curUser.id });
-        var owner = await sails.models.user.findOne({id: folder.user_id });
+        var owner = await sails.models.user.findOne({ id: folder.user_id });
         var notInFolder = [], curUserSetsTitle = [], setInFolderTitle = [], notInFolderSets = [], countTerms = [], owners = [];
-        for(var i = 0; i < curUserSets.length; i++){
+        for (var i = 0; i < curUserSets.length; i++) {
             curUserSetsTitle.push(curUserSets[i].title);
         }
         for (var i = 0; i < setsInFolder.sets.length; i++) {
             setInFolderTitle.push(setsInFolder.sets[i].title);
-            countTerms[i] = await sails.models.card.count({set_id: setsInFolder.sets[i].id});
-			owners[i] = await sails.models.user.findOne({id: setsInFolder.sets[i].user_id});
+            countTerms[i] = await sails.models.card.count({ set_id: setsInFolder.sets[i].id });
+            owners[i] = await sails.models.user.findOne({ id: setsInFolder.sets[i].user_id });
         }
 
-        notInFolder = curUserSetsTitle.filter(object=>{
+        notInFolder = curUserSetsTitle.filter(object => {
             return setInFolderTitle.indexOf(object) == -1;
         })
-        
-        for(var i = 0; i < notInFolder.length; i++){
-            var notInFolderSet = await sails.models.set.findOne({title: notInFolder[i], user_id: curUser.id});
+
+        for (var i = 0; i < notInFolder.length; i++) {
+            var notInFolderSet = await sails.models.set.findOne({ title: notInFolder[i], user_id: curUser.id });
             notInFolderSets.push(notInFolderSet);
         }
 
@@ -52,16 +52,21 @@ module.exports = {
         });
     },
 
-    addSets: async function(req, res){
+    addSets: async function (req, res) {
         var folderId = req.body.folderId;
         var setsCheck = req.body.set;
-        var user = await sails.models.user.findOne({username: req.signedCookies.user.username });
+        var user = await sails.models.user.findOne({ username: req.signedCookies.user.username });
         var setTitles = req.body.setTitle;
-
-        for(var i = 0; i < setTitles.length; i++){
-            if(setsCheck[i] === setTitles[i] || setsCheck === setTitles[i]){
-                var set = await sails.models.set.findOne({title: setTitles[i], user_id: user.id});
-                await sails.models.folder.addToCollection(folderId, 'sets', set.id);
+        var set;
+        if (typeof (setTitles) == 'string') {
+            set = await sails.models.set.findOne({ title: setTitles, user_id: user.id });
+            await sails.models.folder.addToCollection(folderId, 'sets', set.id);
+        } else {
+            for (var i = 0; i < setTitles.length; i++) {
+                if (setsCheck[i] === setTitles[i] || setsCheck === setTitles[i]) {
+                    set = await sails.models.set.findOne({ title: setTitles[i], user_id: user.id });
+                    await sails.models.folder.addToCollection(folderId, 'sets', set.id);
+                }
             }
         }
         res.redirect(`/${user.username}/folders/${folderId}`);
@@ -73,10 +78,10 @@ module.exports = {
         res.redirect(`/${req.signedCookies.user.username}/profile#folders`);
     },
 
-    removeSet: async function(req, res){
+    removeSet: async function (req, res) {
         var folderId = req.params.folderId;
         var setId = req.params.setId;
-        var user = await sails.models.user.findOne({username: req.signedCookies.user.username });
+        var user = await sails.models.user.findOne({ username: req.signedCookies.user.username });
         await sails.models.folder.removeFromCollection(folderId, 'sets', setId);
         res.redirect(`/${user.username}/folders/${folderId}`);
     }
